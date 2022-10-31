e7db7e3700001e6e2d3ae14434d904ccccc0c8dd
FikretDuzceker <fikretduzceker5@gmail.com>
2022-10-31 09:35:23 +0300

    Changed Theme

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,12 @@
 import React from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { loginAction, logoutAction } from "../store/features/login/LoginSlice";
 
 const Login = () => {
   const dispatch = useDispatch();
+  const theme = useSelector((state) => state.themeReducer.value);
   return (
-    <div>
+    <div style={{ color: theme }}>
       <h2>Login</h2>
       <button
         onClick={() => {
--- src/components/Theme.js
+++ src/components/Theme.js
@@ -1,13 +1,14 @@
 import React, { useState } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { setTheme } from "../store/features/theme/ThemeSlice";
 
 const Theme = () => {
   const [color, setColor] = useState("");
+  const theme = useSelector((state) => state.themeReducer.value);
 
   const dispatch = useDispatch();
   return (
-    <div>
+    <div style={{ color: theme }}>
       <h2>Theme</h2>
       <input type="text" onChange={(e) => setColor(e.target.value)} />
       <button
