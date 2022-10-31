f3f952babbe213b63c8a92b1392d9fda141aae86
FikretDuzceker <fikretduzceker5@gmail.com>
2022-10-31 11:15:33 +0300

    Changed login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,16 +1,23 @@
 import React from "react";
+import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { loginAction, logoutAction } from "../store/features/login/LoginSlice";
 
 const Login = () => {
+  const [newUserName, setNewUserName] = useState("");
+  const [newUserSurName, setNewUserSurName] = useState("");
   const dispatch = useDispatch();
   const theme = useSelector((state) => state.themeReducer.value);
   return (
     <div style={{ color: theme }}>
       <h2>Login</h2>
+      name:{" "}
+      <input type="text" onChange={(e) => setNewUserName(e.target.value)} />
+      surname:{" "}
+      <input type="text" onChange={(e) => setNewUserSurName(e.target.value)} />
       <button
         onClick={() => {
-          dispatch(loginAction({ name: "F", surname: "D" }));
+          dispatch(loginAction({ name: newUserName, surname: newUserSurName }));
         }}
       >
         Login
